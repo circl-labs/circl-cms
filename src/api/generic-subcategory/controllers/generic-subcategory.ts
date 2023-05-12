1ae3cd50b42d78fcc6c2a702124155a75802a3d8
Alain Bansais <alain.bansais@circl.fr>
2023-05-12 16:28:33 +0200

    update structure for generic products

---
+++ src/api/generic-subcategory/controllers/generic-subcategory.ts
@@ -0,0 +1,7 @@
+/**
+ * generic-subcategory controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::generic-subcategory.generic-subcategory');
---
+++ src/api/generic-subcategory/services/generic-subcategory.ts
@@ -0,0 +1,7 @@
+/**
+ * generic-subcategory service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::generic-subcategory.generic-subcategory');
