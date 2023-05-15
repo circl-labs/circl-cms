599868b0b4055ba32008ae1e0d7570b74c483897
Alain Bansais <alain.bansais@circl.fr>
2023-05-15 16:31:48 +0200

    fix publicfiles

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,7 +4,7 @@ module.exports = {
         provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
         providerOptions: {
             bucketName: 'circl-data-dev-eu-assets',
-            publicFiles: false,
+            publicFiles: true,
             uniform: false,
             basePath: '',
         },
