8ade86de4522c965fd205e6dc11531340611c996
Alain Bansais <alain.bansais@circl.fr>
2023-05-23 17:17:46 +0200

    add order and customer tables

---
+++ src/api/customer-address/controllers/customer-address.ts
@@ -0,0 +1,7 @@
+/**
+ * customer-address controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::customer-address.customer-address');
---
+++ src/api/customer-address/routes/customer-address.ts
@@ -0,0 +1,7 @@
+/**
+ * customer-address router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::customer-address.customer-address');
