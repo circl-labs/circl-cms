bb4e6aa5d9bc5129803374a3605650809db57720
kodaho <23268819+kodaho@users.noreply.github.com>
2023-09-24 19:20:19 +0200

    fix: move server URL to an environment variable

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://strapi-hwnuo7l2pa-ey.a.run.app/',
+  url: env('URL', ''),
   app: {
     keys: env.array('APP_KEYS'),
   },
