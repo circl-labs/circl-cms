18d4fecfdb0119a6d1a631dbafec33247685bd16
Alain Bansais <alain.bansais@circl.fr>
2023-10-20 10:30:19 +0200

    move missing generic product into new collection

---
+++ src/api/missing-generic-product/routes/missing-generic-product.ts
@@ -0,0 +1,7 @@
+/**
+ * missing-generic-product router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::missing-generic-product.missing-generic-product');
---
+++ src/api/missing-generic-product/services/missing-generic-product.ts
@@ -0,0 +1,7 @@
+/**
+ * missing-generic-product service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::missing-generic-product.missing-generic-product');
