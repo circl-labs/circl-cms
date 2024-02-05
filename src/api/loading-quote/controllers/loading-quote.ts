1ceb6bf958f744600f1c10546acb92aa22ad7e46
Alain Bansais <alain.bansais@circl.fr>
2024-02-05 18:04:06 +0100

    add new quotes table

---
+++ src/api/loading-quote/controllers/loading-quote.ts
@@ -0,0 +1,7 @@
+/**
+ * loading-quote controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::loading-quote.loading-quote');
---
+++ src/api/loading-quote/routes/loading-quote.ts
@@ -0,0 +1,7 @@
+/**
+ * loading-quote router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::loading-quote.loading-quote');
